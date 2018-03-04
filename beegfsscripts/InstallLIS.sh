11c3dc2178013e0820aa66f45d630f83272099be
tonywu70 <tony.wenyong@gmail.com>
2018-03-04 12:17:53 +0800

    Update InstallLIS.sh

--- beegfsscripts/InstallLIS.sh
+++ beegfsscripts/InstallLIS.sh
@@ -7,8 +7,9 @@ if [[ $(id -u) -ne 0 ]] ; then
     echo "Must be run as root"
     exit 1
 fi
-wget 'https://download.microsoft.com/download/7/6/B/76BE7A6E-E39F-436C-9353-F4B44EF966E9/lis-rpms-4.1.3-1.tar.gz'
-tar xvzf lis-rpms-4.1.3-1.tar.gz
+wget 'https://download.microsoft.com/download/6/8/F/68FE11B8-FAA4-4F8D-8C7D-74DA7F2CFC8C/lis-rpms-4.2.4.tar.gz'
+#wget 'https://download.microsoft.com/download/7/6/B/76BE7A6E-E39F-436C-9353-F4B44EF966E9/lis-rpms-4.1.3-1.tar.gz'
+tar xvzf lis-rpms-4.2.4.tar.gz
 cd LISISO
 ./install.sh
 shutdown -r +1 &
