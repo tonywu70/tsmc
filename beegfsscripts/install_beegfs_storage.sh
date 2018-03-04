47e8cc613e9e806b1d6553a6827b46d8a262d498
tonywu70 <tony.wenyong@gmail.com>
2018-03-04 12:10:54 +0800

    Update install_beegfs_storage.sh

--- beegfsscripts/install_beegfs_storage.sh
+++ beegfsscripts/install_beegfs_storage.sh
@@ -212,12 +212,14 @@ install_beegfs_repo()
 {
     # Install BeeGFS repo
     #wget -O beegfs-rhel7.repo http://www.beegfs.com/release/beegfs_2015.03/dists/beegfs-rhel7.repo
-    wget -O beegfs-rhel7.repo http://www.beegfs.com/release/beegfs_6/dists/beegfs-rhel7.repo
+    wget -O beegfs-rhel7.repo https://www.beegfs.io/release/beegfs_7/dists/beegfs-rhel7.repo
+    #wget -O beegfs-rhel7.repo http://www.beegfs.com/release/beegfs_6/dists/beegfs-rhel7.repo
 	
 
     mv beegfs-rhel7.repo /etc/yum.repos.d/beegfs.repo
     #rpm --import http://www.beegfs.com/release/beegfs_2015.03/gpg/RPM-GPG-KEY-beegfs
-    rpm --import http://www.beegfs.com/release/beegfs_6/gpg/RPM-GPG-KEY-beegfs
+    rpm --import https://www.beegfs.io/release/beegfs_7/gpg/RPM-GPG-KEY-beegfs
+    #rpm --import http://www.beegfs.com/release/beegfs_6/gpg/RPM-GPG-KEY-beegfs
 
 }
 
