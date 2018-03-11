0d8e7af950f7fbbcd513b8247efe26ebca467126
tonywu70 <tony.wenyong@gmail.com>
2018-03-11 11:55:42 +0800

    Update install_beegfs.sh

--- beegfsscripts/install_beegfs.sh
+++ beegfsscripts/install_beegfs.sh
@@ -334,13 +334,15 @@ setup_user()
 
 install_LIS()
 {
-    wget 'https://download.microsoft.com/download/6/8/F/68FE11B8-FAA4-4F8D-8C7D-74DA7F2CFC8C/lis-rpms-4.2.4.tar.gz'
+    #wget 'https://download.microsoft.com/download/6/8/F/68FE11B8-FAA4-4F8D-8C7D-74DA7F2CFC8C/lis-rpms-4.2.4.tar.gz'
 
-     tar xvzf lis-rpms-4.2.4.tar.gz
+     #tar xvzf lis-rpms-4.2.4.tar.gz
 
-     cd LISISO
+     #cd LISISO
 
-     ./install.sh
+     #./install.sh
+     yum update
+     yum install microsoft-hyper-v
 }
 
 SETUP_MARKER=/var/local/install_beegfs.marker
